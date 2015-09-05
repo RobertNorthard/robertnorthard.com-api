8b2741bbb6f5bf9fef4f1a8febaaa93d1fd5194a
Robert Northard <robertnorthard@googlemail.com>
2015-09-05 22:21:11 +0100

    Initialised repo with SpringBoot/API structure

---
+++ src/main/java/robertnorthard/com/api/controllers/HealthController.java
@@ -0,0 +1,19 @@
+
+package robertnorthard.com.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Expose health status API
+ * @author robertnorthard
+ */
+
+@RestController
+public class HealthController {
+    @RequestMapping("/health")
+    public String health(){
+        //TODO: Implement Health Status Check
+        return "{\"Status\":\"UP\"}";
+    }
+}
