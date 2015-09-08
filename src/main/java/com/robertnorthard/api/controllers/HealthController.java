666a628a0f6ef0a56475012433133e4fcfc0bced
Robert Northard <robertnorthard@googlemail.com>
2015-09-08 22:41:09 +0100

    Correct health context mapping

--- src/main/java/com/robertnorthard/api/controllers/HealthController.java
+++ src/main/java/com/robertnorthard/api/controllers/HealthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/health")
 public class HealthController {
     @RequestMapping(method=RequestMethod.GET)
     public String health(){
