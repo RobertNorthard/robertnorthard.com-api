667204d4db264a181b8d1a0f6ebf16d3354d4570
Robert Northard <robertnorthard@googlemail.com>
2016-06-22 22:49:26 +0100

    Added authenticate and find user methods to UserFacade

--- src/main/java/com/robertnorthard/api/service/UserFacade.java
+++ src/main/java/com/robertnorthard/api/service/UserFacade.java
@@ -1,5 +1,6 @@
 package com.robertnorthard.api.service;
 
+import com.robertnorthard.api.model.security.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
@@ -8,5 +9,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * @author robertnorthard
  */
 public interface UserFacade extends UserDetailsService {
+    
+    /**
+     * Find user with specified name
+     *
+     * @param username user to find
+     * @return user, null if user not found.
+     */
+    public User findByUsername(String username);
+    
+    /**
+     * Return user if authenticated else null
+     *
+     * @param user user to authenticate with
+     * @return user if authenticated else null
+     */
+    public User authenticate(User user);
     
 }
